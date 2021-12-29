c562e8e549b577713fee15607e86c0b0163ec609
Simone Ceredi <ceredi.simone.iti@gmail.com>
2021-12-29 17:15:58 +0100

    Added analogRead from first esp

--- TestWS/scripts/blink2esp.js
+++ TestWS/scripts/blink2esp.js
@@ -15,53 +15,67 @@
 const boards = ['192.168.1.12', '192.168.1.13'];
 const port = 3030;
 
-
-let Firmata = require("firmata").Board;
-let EtherPortClient = require("etherport-client").EtherPortClient;
-let board1 = new Firmata(new EtherPortClient({
-    host: boards[0],
-    port: port
-}));
-let board2 = new Firmata(new EtherPortClient({
-    host: boards[1],
-    port: port
-}));
+let Firmata = require('firmata').Board;
+let EtherPortClient = require('etherport-client').EtherPortClient;
+let board1 = new Firmata(
+    new EtherPortClient({
+        host: boards[0],
+        port: port,
+    })
+);
+let board2 = new Firmata(
+    new EtherPortClient({
+        host: boards[1],
+        port: port,
+    })
+);
 let start = Date.now();
 
-board1.on("ready", function () {
+board1.on('ready', function () {
     let connectionTime = Date.now() - start;
-    console.log("READY!, connection to " + boards[0] + " took: " + connectionTime + 'ms');
-    
+    console.log('READY!, connection to ' + boards[0] + ' took: ' + connectionTime + 'ms');
 
     let state = 1;
     let lastVal = 0;
 
     this.pinMode(16, this.MODES.OUTPUT);
 
-    setInterval(function () {
-        // blinks the blue LED on a HUZZAH ESP8266 board
-        // for other boards, wire an LED to pin 2 or change
-        // the pin number below
-        this.digitalWrite(16, (state ^= 1));
-    }.bind(this), 100);
+    setInterval(
+        function () {
+            // blinks the blue LED on a HUZZAH ESP8266 board
+            // for other boards, wire an LED to pin 2 or change
+            // the pin number below
+            this.digitalWrite(16, (state ^= 1));
+        }.bind(this),
+        100
+    );
 
+    setInterval(
+        function () {
+            this.analogRead(0, (value) => console.log('Read A0 from ' + boards[0] + ', value -> ' + value));
+        }.bind(this),
+        100
+    );
 });
 
-board2.on("ready", function () {
+board2.on('ready', function () {
     let connectionTime = Date.now() - start;
-    console.log("READY!, connection to " + boards[1] + " took: " + connectionTime + 'ms');
-    
+    console.log('READY!, connection to ' + boards[1] + ' took: ' + connectionTime + 'ms');
 
     let state = 1;
     let lastVal = 0;
 
     this.pinMode(16, this.MODES.OUTPUT);
 
-    setInterval(function () {
-        // blinks the blue LED on a HUZZAH ESP8266 board
-        // for other boards, wire an LED to pin 2 or change
-        // the pin number below
-        this.digitalWrite(16, (state ^= 1));
-    }.bind(this), 200);
+    setInterval(
+        function () {
+            // blinks the blue LED on a HUZZAH ESP8266 board
+            // for other boards, wire an LED to pin 2 or change
+            // the pin number below
+            this.digitalWrite(16, (state ^= 1));
+        }.bind(this),
+        200
+    );
 
+    console.log(board2.analog);
 });
