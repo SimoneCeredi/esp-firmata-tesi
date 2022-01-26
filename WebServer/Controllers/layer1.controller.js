3f2e0d973511f437c6f3dff275ff9ab21c4b3c44
Simone Ceredi <ceredi.simone.iti@gmail.com>
2022-01-26 11:35:22 +0100

    Added layer1 buttonread

--- WebServer/Controllers/layer1.controller.js
+++ WebServer/Controllers/layer1.controller.js
@@ -1,4 +1,5 @@
 const model = require('../Models/layer1.model');
+const layer0model = require('../Models/layer0.model');
 
 const buttonEvent = (req, res) => {
     const ip = req.query['ip'];
@@ -10,6 +11,16 @@ const buttonEvent = (req, res) => {
         .catch((err) => res.status(400).send(err));
 };
 
+const buttonRead = (req, res) => {
+    const ip = req.query['ip'];
+    const pin = req.query['pin'];
+    layer0model
+        .digitalRead(ip, pin)
+        .then(({ code, message }) => res.status(code).send(message))
+        .catch((err) => res.status(400).send(err));
+};
+
 module.exports = {
     buttonEvent,
+    buttonRead,
 };
--- WebServer/Routes/layer1.routes.js
+++ WebServer/Routes/layer1.routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const controller = require('../Controllers/layer1.controller');
 
 router.get('/buttonevent', controller.buttonEvent);
+router.get('/buttonread', controller.buttonRead);
 
 module.exports = router;
